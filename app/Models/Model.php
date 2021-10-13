9e2d7beafe55ecde47b9efcf4ecd3c9e8e3a95a4
wangchengtao <915129420@qq.com>
2021-10-13 09:09:06 +0800

    时间格式处理

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
+use App\Models\Traits\HasDateTimeFormatter;
 use Illuminate\Database\Eloquent\Model as BaseModel;
 
 class Model extends BaseModel
 {
-    //
+    use HasDateTimeFormatter;
 }
