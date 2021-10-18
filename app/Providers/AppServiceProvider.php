8f59ee6db87c8c51a5d84cc28ef14e5872f82257
wangchengtao <915129420@qq.com>
2021-10-18 15:27:51 +0800

    添加时间验证器

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,17 @@
 
 namespace App\Providers;
 
+use App\Validators\PhoneValidator;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected $validators = [
+        'phone' => PhoneValidator::class,
+    ];
+
     /**
      * Register any application services.
      *
@@ -25,5 +31,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Carbon::setLocale('zh');
+
+        $this->registerValidators();
+    }
+
+    protected function registerValidators()
+    {
+        foreach ($this->validators as $rule => $validator) {
+            Validator::extend($rule, "{$validator}@validate");
+        }
     }
 }
