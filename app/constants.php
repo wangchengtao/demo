41dca09312636575a20d1a11ca44500acb745971
wangchengtao <915129420@qq.com>
2024-01-24 09:40:45 +0800

    去掉不必要的中间件

--- app/constants.php
+++ app/constants.php
@@ -1,7 +1,7 @@
 <?php
-define('SUCCESS', '000000');
-define('FAILED', '300000');
-define('NOT_EXISTS', '300000');
-define('NOT_ALLOWED', '300000');
-define('EXPIRED', '300000');
-define('PARAM_ERROR', '300000');
+const SUCCESS = '000000';
+const FAILED = '300000';
+const NOT_EXISTS = '300000';
+const NOT_ALLOWED = '300000';
+const EXPIRED = '300000';
+const PARAM_ERROR = '300000';
